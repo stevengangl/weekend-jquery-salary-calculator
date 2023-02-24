e16f8f8d10f88338c4e30de4a1c0245acd3890f1
Steven Gangl <steven.h.gangl@gmail.com>
2023-02-24 16:28:53 -0600

    input form comes back onto dom when submit button pressed

--- calculator.js
+++ calculator.js
@@ -1,13 +1,59 @@
 $(document).ready(readyNow);
-
+//variables will be stores
+let monthlyCost = [];
+let employees = [];
 
 //add Readynow() 
-function readyNow(){
+function readyNow() {
     console.log('DOM is loaded')
-// add a listener for the "submitEmployeeInfoBtn" button
-$("#submitEmployeeInfoBtn").on('click', employeeInfo)
+    // add a listener for the "submitEmployeeInfoBtn" button
+    $("#submitEmployeeInfoBtn").on('click', addEmployeeInfo)
 }
 
-function employeeInfo(){
+function addEmployeeInfo() {
     console.log('in employeeInfo()')
+    const firstNameInputValue = $("#firstNameInput").val();
+    const lastNameInputValue = $("#lastNameInput").val();
+    const idInputValue = $("#idInput").val();
+    const titleInputValue = $("#titleInput").val();
+    const salaryInputValue = $("#salaryInput").val();
+    console.log(`
+    First Name:${firstNameInputValue}
+    Last Name:${lastNameInputValue}
+    ID:${idInputValue}
+    Title:${titleInputValue}
+    Salary:${salaryInputValue}
+    `);
+    const employeeInfo = {
+        first: firstNameInputValue,
+        last: lastNameInputValue,
+        ID: idInputValue,
+        title: titleInputValue,
+        salary: salaryInputValue
+    };
+    employees.push(employeeInfo);
+    console.log('stuff in employees', employeeInfo)
+
+    render();
+}
+
+function render() {
+$('#employeeInfoForm').empty();
+//im trying to reset my form after i click the button
+for(let people of employees){
+    // trying to update the dom with the submitted info
+    $('#employeeInfoForm').append(`
+ 
+    <tr>
+        <th>${people.first}</th>
+        <th>${people.last}</th>
+        <th>${people.ID}</th>
+        <th>${people.title}</th>
+        <th>${people.salary}</th>
+        <button id ='deleteBtn'>Delete</button>people
+    </tr>
+    
+   
+    `)
+}
 }
