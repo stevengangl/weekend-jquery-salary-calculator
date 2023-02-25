aee59bf11f266840c136b11ba399453c20666b56
Steven Gangl <steven.h.gangl@gmail.com>
2023-02-25 17:11:36 -0600

    added background to total monthly costs if it exceeds 20,000

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ let employees = [];
 let employeeSalary = 0;
 
 // KEY
+// ⚪️ comments to help me
 // 🟢 = comments on what is confusing
 // 🔴 problem areas within my code
 // 🔵 fixed problem area
@@ -15,7 +16,7 @@ let employeeSalary = 0;
 //1️⃣ clear the input fields✅
 //2️⃣ calculate monthly costs✅
 //3️⃣ append monthly costs to the dom at below the table✅
-//4️⃣ If totalMonthlyCost > $20,000, add red background to the total monthly cost.
+//4️⃣ If totalMonthlyCost > $20,000, add red background to the total monthly cost.✅
 //5️⃣ Create a delete button that removes an employee from the DOM
 //6️⃣ Fix stuff on DOM so it has correct Data
 
@@ -23,19 +24,19 @@ let employeeSalary = 0;
 //⭐️
 function readyNow() {
     console.log('dom is loaded')
-    // need to add click event that calls a function()
-    // need to make a function to add the info 
-    // also need a listener for when the button is clicked 
+    //⚪️need to add click event that calls a function()
+    //⚪️need to make a function to add the info 
+    //⚪️also need a listener for when the button is clicked 
     $("#submitInfoBtn").on('click', addEmployeeInfo) 
 
 }
-// function to grab info from input fields 
-// function needs to add new info onto the DOM
+//⚪️function to grab info from input fields 
+//⚪️function needs to add new info onto the DOM
 //⭐️
 function addEmployeeInfo() {
     console.log('inside of addEmployeeInfo()');
 
-    // Have to make variables to get the input info
+    //⚪️Have to make variables to get the input info
     const firstName = $("#firstNameInput").val();
     const lastName = $("#lastNameInput").val();
     const id = $("#idInput").val();
@@ -50,6 +51,7 @@ function addEmployeeInfo() {
     // Title: ${title}
     // Salary: ${salary}
     // `);
+    if( firstName && lastName && id && title && salary) {
 
     let employeeInfo = {
         employeeFirstName: firstName,
@@ -62,17 +64,20 @@ function addEmployeeInfo() {
     employees.push(employeeInfo);
 
     console.log(' employee array ', employees);
-    //render puts the info onto the DOM
-    // than this resets the input form
+    //⚪️render puts the info onto the DOM
+    //⚪️than this resets the input form
     //🔵I had render above the other functions so it wasnt working🔵
     resetInputField();
     totalMonthlyCost();
     render();//🔴this was in the wrong spot but it works now!!
+} else {
+    alert('Missing Inputs!')
+}
 }
 //⭐️
 function resetInputField() {
-    //this is a "setter?" its job is to empty the input field
-    // it basically adds an empty string to the input form
+    //⚪️this is a "setter?" its job is to empty the input field
+    // ⚪️it basically adds an empty string to the input form
     $('#firstNameInput').val('');
     $("#lastNameInput").val('');
     $("#idInput").val('');
@@ -84,13 +89,13 @@ function resetInputField() {
 function totalMonthlyCost(){
     console.log('in totalMonthlyCost()');
     console.log('this is the employee array:', employees);
-    //checking to see if the function is grabbing the salary
-    //the index is because this object is in the global array
+    //⚪️checking to see if the function is grabbing the salary
+    //⚪️the index is because this object is in the global array
     console.log('checking salary of employee array', employees[0].employeeId);//🟢I needed help on this, i didnt realize i needed to access the index
 
-// the property "employeeSalary" of the object "employeeInfo"   is what i need to target to get the salary, 
-//made a variable named employeeSalary
-//that variable stores the salary by accessing its property within the object
+// ⚪️the property "employeeSalary" of the object "employeeInfo"   is what i need to target to get the salary, 
+//⚪️made a variable named employeeSalary
+//⚪️that variable stores the salary by accessing its property within the object
 
 //🟢this for loop took me about 1 hour to figure out...
 let variableA = 0;           // 🟢I dont have a good grasp on this  for loop concept
@@ -108,7 +113,7 @@ function render() {
     // $('#employeeInfoForm').empty();
     for (let info of employees) {
         console.log('individual info', info)
-        // need to append the info to the DOM
+        // ⚪️need to append the info to the DOM
         $("#employeeInfoTable").append(`
     <tr>
         <td>${info.employeeFirstName}</td>
@@ -125,5 +130,12 @@ function render() {
     `);
     };
     //🔴the DOM is not rendering this correctly, its 1 behind
+    //🔵problem fixed, render was in the wrong spot in the code above
     $("#employeeMonthlyCost").html(`total monthly costs <p>${employeeSalary}</p>`);
+
+    //⚪️ I need to make a conditional for the total monthly costs for step 4
+
+    if(employeeSalary > 20000){
+        $('#employeeMonthlyCost').addClass('red');
+    }
 }
